daef962c2c19b3750530ba1fc6041af13935e4ab
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-26 15:24:03 -0300

    get released movie list from API

--- src/app/movie/movie-tile/movie-tile.component.ts
+++ src/app/movie/movie-tile/movie-tile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MoviesService } from 'src/app/shared/movies.service';
 
 @Component({
   selector: 'app-movie-tile',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-tile.component.scss']
 })
 export class MovieTileComponent implements OnInit {
+  @Input() movie: any
+  @Input() movieID: number
+  @Input() coverPath: string
+  cover: string
 
-  constructor() { }
+  constructor(private mService: MoviesService) {
+  }
 
   ngOnInit(): void {
+    this.cover = this.mService.imagePath + this.coverPath
   }
+  
 
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MoviesService } from '../shared/movies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
+  releasedMovies = []
+  subRelease: Subscription
 
-  constructor() { }
+  constructor(private mService: MoviesService) { }
 
   ngOnInit(): void {
+    this.subRelease = this.mService.getReleases().subscribe(response => {
+      this.releasedMovies = response.results
+    });
+  }
+
+  ngOnDestroy() {
+    this.subRelease.unsubscribe()
   }
 
 }
