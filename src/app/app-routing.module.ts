e70e720857f5659b13852afd25541d24206636f4
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-27 16:54:45 -0300

    fix app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { AllCategoriesComponent } from './all-categories/all-categories.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: 'categories', component: AllCategoriesComponent},
+  { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
