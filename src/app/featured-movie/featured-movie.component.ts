6a3bebf7d7151dfe495c9c378624e715a91d5f44
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-28 13:44:28 -0300

    change heart button color on click

--- src/app/featured-movie/featured-movie.component.ts
+++ src/app/featured-movie/featured-movie.component.ts
@@ -11,10 +11,15 @@ export class FeaturedMovieComponent implements OnInit, OnDestroy {
   backdrop: string
   featuredMovie: any
   genres = []
+  heartClass: string[]
+  isFavorite: boolean
   subscription: Subscription
   title: string
 
-  constructor(private mService: MoviesService) {}
+  constructor(private mService: MoviesService) {
+    this.isFavorite = false
+    this.heartClass = ['fa fa-heart-o'];
+  }
 
   ngOnInit(): void {
     this.subscription = this.mService.getMovie(458156).subscribe(res => {
@@ -24,6 +29,12 @@ export class FeaturedMovieComponent implements OnInit, OnDestroy {
       this.backdrop = this.mService.getImagePath() + this.featuredMovie.backdrop_path
     });
     
+  }
+
+  //change heart color when favorite
+  toFavorite() {
+    this.isFavorite = !this.isFavorite
+    this.heartClass = this.isFavorite ? ['fa fa-heart'] : ['fa fa-heart-o'];
   }
 
   //I really didn't like the title, I'll break it to be more aesthetic >:(
