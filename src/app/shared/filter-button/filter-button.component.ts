84b27da1da2c5c2f6673b0644ef604c9e3e23bea
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-29 14:37:18 -0300

    add filter button component in the mobile header

--- src/app/shared/filter-button/filter-button.component.ts
+++ src/app/shared/filter-button/filter-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filter-button',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-button.component.scss']
 })
 export class FilterButtonComponent implements OnInit {
+  route: Router
 
-  constructor() { }
+  constructor(router: Router) {
+    this.route = router
+  }
 
   ngOnInit(): void {
   }
